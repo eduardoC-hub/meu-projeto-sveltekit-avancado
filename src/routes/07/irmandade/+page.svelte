20fc7718c6446529346be4d20ef33ab44c7e41cb
eduardoC-hub <eduardomarquiori99@gmail.com>
2025-06-27 08:27:20 -0400

    a

--- src/routes/07/irmandade/+page.svelte
+++ src/routes/07/irmandade/+page.svelte
@@ -1,24 +1,24 @@
 <script>
-    let {form} = $state();
+    let {form} = $props();
 </script>
 <form method="post">
     nome: <input name="nome" type="text" placeholder="nome do aprendiz"/> 
     <br>
     nascimento: <input name="nascimento" type="date"/> 
     <br>
-    força: <input name="forca" type="number" min="1" max="100"/>
+    força: <input name="forca" type="number" min="1" max="10"/>
     <br>
-    inteligencia: <input name="inteligencia" type="number" min="1" max="100"/>
+    inteligência: <input name="inteligencia" type="number" min="1" max="10"/>
     <br>
-    destreza: <input name="destreza" type="number" min="1" max="100"/> 
+    destreza: <input name="destreza" type="number" min="1" max="10"/> 
     <br>
     conhecimento de magia: <input name="magia" type="checkbox"/>
     <br>
     ferramentas de artesao: <input name="ferramentas" type="checkbox"/> 
     <br> 
-    <button formaction="?/guerreiros">inscever na ordem dos guerreiros</button>
-    <button formaction="?/magos">inscever na circulo dos magos</button>
-    <button formaction="?/artesaos">inscever na guilda dos artesaos</button>
+    <button formaction="?/guerreiros">inscrever na ordem dos guerreiros</button>
+    <button formaction="?/magos">inscrever na circulo dos magos</button>
+    <button formaction="?/artesaos">inscrever na guilda dos artesaos</button>
 </form>
 {#if form?.error}
 <p style="color:red">{form.error}</p>
---
+++ src/routes/07/irmandade/artesaos/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Parabéns você se tornou um artesão!</h1>
